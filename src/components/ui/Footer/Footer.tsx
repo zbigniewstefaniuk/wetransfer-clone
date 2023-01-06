d59eee630c57ffb37480e3d5e2d5bb20c9a0435f
Zbigniew Stefaniuk <zbyszek791@hotmail.com>
2023-01-06 18:39:52 +0100

    fix(Footer): position

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,9 +1,12 @@
 function Footer() {
   return (
-    <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <footer className="absolute bottom-0 inset-x-0 p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © {new Date().getFullYear()}{" "}
-        <a href="https://flowbite.com/" className="hover:underline">
+        <a
+          href="https://github.com/zbigniewstefaniuk"
+          className="hover:underline"
+        >
           Mammoth™
         </a>
         . All Rights Reserved.
