0551abe4100445958e22a604cf80483c000a1bde
Zbigniew Stefaniuk <zbyszek791@hotmail.com>
2023-01-06 18:35:53 +0100

    comment code

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -2,10 +2,10 @@ import supabase from "@/lib/Supabse/supabaseCLient";
 import { FC } from "react";
 
 const Login: FC = () => {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: "example@email.com",
-    password: "example-password",
-  });
+  //   const { data, error } = await supabase.auth.signInWithPassword({
+  //     email: "example@email.com",
+  //     password: "example-password",
+  //   });
   return <div>Login</div>;
 };
 
