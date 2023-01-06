a6d032a1c3653a7a99d35bc9caf69ee5b650d200
zayn <59846591+zbigniewstefaniuk@users.noreply.github.com>
2023-01-06 20:42:45 +0100

    feat: react hook form (#1)

---
+++ src/hooks/useAuthStateChange.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { AuthChangeEvent, Session } from "@supabase/supabase-js";
+import supabase from "@/lib/Supabase";
+
+const useAuthStateChange = () => {
+  const [authState, setAuthState] = useState<{
+    event: AuthChangeEvent | null;
+    session: Session | null;
+  }>({
+    event: null,
+    session: null,
+  });
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      console.log(event, session);
+      setAuthState({
+        event,
+        session,
+      });
+    });
+  }, []);
+
+  return authState;
+};
+
+export default useAuthStateChange;
