f10133609a04c8ca83c9a983f4a3f7153b5048de
Zbigniew Stefaniuk <zbyszek791@hotmail.com>
2023-01-08 09:09:49 +0100

    fix: input forward ref

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -4,12 +4,13 @@ interface Props {
   type?: InputHTMLAttributes<HTMLInputElement>["type"];
   name: string;
   label: string;
-  placeholder: string;
+  placeholder?: string;
+  onChange: InputHTMLAttributes<HTMLInputElement>["onChange"];
   ref: ForwardedRef<HTMLInputElement>;
 }
 
 const Input: FC<Props> = forwardRef(
-  ({ type = "text", name, label, placeholder }, ref) => (
+  ({ type = "text", name, label, placeholder, onChange }, ref) => (
     <>
       <label
         htmlFor={name}
@@ -23,6 +24,7 @@ const Input: FC<Props> = forwardRef(
         name={name}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeholder}
+        onChange={onChange}
         ref={ref}
       />
     </>
